0ece91743dce25683dbbd4690822e01a4772a03d
Arthur Oxenhorn <arthuro@cs.huji.ac.il>
2022-11-04 18:15:35 +0200

    added all requirements

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-contactButton = document.querySelector("#contact button");
+contactButton = document.querySelector("#fishVideoButton");
+video = document.querySelector("#fishVideo");
+image1 = document.querySelector("#floatingFish1");
+image2 = document.querySelector("#floatingFish2");
+colorsButton = document.querySelector("#changeColorsButton");
+container = document.querySelector("#container");
+title = document.querySelector("#title");
 
-contactButton.addEventListener("click", function contactClickhandler(el) {
-  alert("Not implemented");
+contactButton.addEventListener("click", function contactClickhandler() {
+    if (video.style.display == 'none') {
+        contactButton.textContent = '⏩HIDE VIDEO⏪';
+        image1.style.display = 'block';
+        image2.style.display = 'block';
+        video.style.display = 'block';
+    } else {
+        contactButton.textContent = '⏩OPEN VIDEO⏪';
+        image1.style.display = 'none';
+        image2.style.display = 'none';
+        video.style.display = 'none';
+    }
+});
+
+colorsButton.addEventListener("click", function colorsClickhandler() {
+    const randomColor = () => Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor();
+    container.style.color = randomColor();
+    title.style.color = randomColor();
 });
